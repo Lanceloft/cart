ce20823cca97a4f17a1b5a669edfa13319636db7
baymaxxx <81243232@qq.com>
2016-03-07 18:23:04 +0800

    Default

--- cart/routes/admin.js
+++ cart/routes/admin.js
@@ -1,11 +1,11 @@
 module.exports = function(app){
     app.get('/admin',function(req,res){
         if(req.session.user && req.session.user.admin){
-            var Carts = global.dbHelper.getModel('cart');
+            var Commodities = global.dbHelper.getModel('commodity');
             var User = global.dbHelper.getModel('user');
-            Carts.find({},function(err,docs1){
+            Commodities.find({},function(err,docs1){
                 User.find({},function(err,docs2){
-                    res.render('admin',{carts:docs1,users:docs2});
+                    res.render('admin',{commoditys:docs1,users:docs2});
                 });
             });
 
@@ -18,6 +18,23 @@ module.exports = function(app){
             var User = global.dbHelper.getModel('user');
             var uname =  req.body.userName;
             User.remove({name:uname},function(err,doc){
+                if(doc > 0){
+                    res.redirect('/admin');
+                }
+            });
+        }else {
+            req.session.error = '请重新登录';
+            res.redirect('/adminLogin')
+        }
+    });
+    app.post('/updateCommodity',function(req,res){
+        if(req.session.user && req.session.user.admin) {
+            var Commodity = global.dbHelper.getModel('commodity');
+            var oname = req.body.oname,
+                nname =  req.body.nname,
+                price = req.body.price,
+                imgSrc = req.body.imgSrc;
+            Commodity.update({name:oname},{$set:{name:nname,price:price,imgSrc:imgSrc}},function(err,doc){
                 if(doc > 0){
                     res.redirect('/admin');
                 }
