995a13bb4a8b40af042b8fd3af73c9f6be0425e7
baymaxxx <81243232@qq.com>
2016-03-23 17:54:12 +0800

    使用gulp

--- cart/gulpfile.js
+++ cart/gulpfile.js
@@ -1,94 +1,143 @@
-'use strict';
 
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    source = require('vinyl-source-stream'),
-    browserify = require('browserify'),
-    concat = require('gulp-concat'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    refresh = require('gulp-livereload'),
-    nodemon = require('gulp-nodemon');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var sass = require('gulp-ruby-sass');
+var _if = require('gulp-if');
+var isWindows = /^win/.test(require('os').platform());
+var lr;
+var EXPRESS_PORT = 3000;
+var EXPRESS_ROOT = __dirname;
+var LIVERELOAD_PORT = 35729;
 
-var expressServer = require('./server');
-gulp.task('serve_', function() {
-    console.log('Server');
-    expressServer.startServer();
+if(isWindows){
+    gutil.log(gutil.colors.bgGreen("Is it Windows? " + isWindows));
+}
+
+// 	LIVE RELOAD //
+
+function startLiveReload(){
+    lr = require('tiny-lr')();
+    lr.listen(LIVERELOAD_PORT);
+}
+
+
+// EXPRESS //
+
+function startExpress(){
+
+    // Dependencies for Express
+
+    var express = require('express');
+    var path = require('path');
+    var bodyParser = require('body-parser');
+    var mongoose = require('mongoose');
+    var multer = require('multer');
+    var session = require('express-session');
+    var crypto = require('crypto');
+    var app = express();
+//全局
+    global.dbHelper = require('./common/dbHelper');
+    global.db = mongoose.connect('mongodb://127.0.0.1:27017/cart');
+
+    app.use(session({
+        secret:'secret',
+        cookie:{
+            maxAge:1000 * 60 * 60 * 24 * 30
+        }
+    }));
+// view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('html',require('ejs').renderFile);
+    app.set('view engine', 'html');
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(multer());
+
+//静态资源
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use(function(req,res,next){
+        res.locals.user = req.session.user;
+        var err = req.session.error;
+        res.locals.message = '';
+        if(err){
+            res.locals.message = '<div style="position: absolute;top:0;color:red;right: 0;">' + err + '</div>';
+        }
+        next();
+    });
+    require('./routes')(app);
+    app.get('/',function(req,res){
+        res.render('login');
+    });
+
+    app.listen(3000);
+
+}
+
+
+// NOTIFY LIVE-RELOAD //
+
+function notifyLiveReload(event) {
+    var fileName = require('path').relative(EXPRESS_ROOT+'/public', event.path);
+
+    lr.changed({
+        body: {
+            files: [fileName]
+        }
+    });
+}
+
+// RELOAD //
+
+function reload(sass,autoprefixer,notify){
+
+    return gulp.src('public/css/**/*.scss')
+        .pipe(sass({style: 'compressed'}))
+        .pipe(autoprefixer('last 15 versions'))
+        .pipe(gulp.dest('public/css'))
+        .pipe(_if(!isWindows, notify({ message: 'That\'s All Folks!'})));
+}
+
+//// UGLIFY //
+//
+//function jsUglify(uglify, concat, gutil){
+//    gutil.log(gutil.colors.bgBlue('Uglifying your JavaScript!'));
+//    return gulp.src(EXPRESS_ROOT+'/public/js/**/*.js')
+//        .pipe(uglify({ mangle: true, compress: true}))
+//        .pipe(concat('main.min.js'))
+//        .pipe(gulp.dest(EXPRESS_ROOT+'/public/javascripts/min/'));
+//}
+
+
+// GULP //
+
+gulp.task('default', function(){
+    startExpress();
+    startLiveReload();
+    //jsUglify(uglify, concat, gutil);
+    reload(sass, autoprefixer, notify);
+
+    gulp.watch('views/**/*.html', notifyLiveReload);
+
+    gulp.watch('public/css/**/*.css', notifyLiveReload);
+
+    gulp.watch('public/js/**/*.js', notifyLiveReload);
+
+
+    //gulp.watch('public/javascripts/*.js', function(){
+    //    return jsUglify(uglify, concat, gutil);
+    //});
+
+    gulp.watch('public/css/**/*.scss', function(){
+
+        return reload(sass, autoprefixer, notify);
+    });
+
+
 });
-
-gulp.task('serve', function () {
-    nodemon({ script: 'server.js', ext: 'json js', ignore: ['public/*', 'client/*'] })
-        .on('change', ['lint'])
-        .on('restart', function () {
-            console.log('Restarted webserver')
-        });
-});
-
-// Dev task
-gulp.task('dev', ['views', 'styles', 'lint', 'browserify', 'watch'], function() {});
-
-// JSLint task
-gulp.task('lint', function() {
-    gulp.src('client/scripts/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-// Styles task
-gulp.task('styles', function() {
-    gulp.src('client/styles/*.scss')
-        // The onerror handler prevents Gulp from crashing when you make a mistake in your SASS
-        .pipe(sass({onError: function(e) { console.log(e); } }))
-        // Optionally add autoprefixer
-        .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
-        // These last two should look familiar now :)
-        .pipe(gulp.dest('public/css/'));
-});
-
-// Browserify task
-gulp.task('browserify', function() {
-    var bundleStream = browserify({
-        entries: ['./client/scripts/main.js'],
-        debug: true
-    }).bundle().pipe(source('core.js'));
-    return bundleStream.pipe(gulp.dest('./public/js'));
-});
-
-// Views task
-gulp.task('views', function() {
-    // Get our index.html
-    gulp.src('client/index.html')
-        // And put it in the public folder
-        .pipe(gulp.dest('public/'));
-
-    // Any other view files from client/views
-    gulp.src('client/views/**/*')
-        // Will be put in the public/views folder
-        .pipe(gulp.dest('public/views/'));
-});
-
-gulp.task('watch', ['serve', 'lint'], function() {
-    // Start live reload server
-    refresh.listen();
-
-    // Watch our scripts, and when they change run lint and browserify
-    gulp.watch(['client/scripts/*.js', 'client/scripts/**/*.js'],[
-        'lint',
-        'browserify'
-    ]);
-
-    // Watch our sass files
-    gulp.watch(['client/styles/**/*.scss'], [
-        'styles'
-    ]);
-
-    // Watch view files
-    gulp.watch(['client/**/*.html'], [
-        'views'
-    ]);
-
-    gulp.watch('./public/**').on('change', refresh.changed);
-
-});
-
-gulp.task('default', ['dev']);
